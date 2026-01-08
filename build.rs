b24cb08ca4bae79021175ae0e454515f10b1cdca
Mads Marquart <mads@marquart.dk>
2026-01-08 18:03:23 +0100

    Slightly improve cross-compilation

--- build.rs
+++ build.rs
@@ -1,13 +1,12 @@
 fn main() {
-    // The script doesn't depend on our code.
+    // The build script doesn't depend on our code.
     println!("cargo:rerun-if-changed=build.rs");
 
-    let host_target = std::env::var_os("TARGET").unwrap();
-    println!("cargo::rustc-env=HOST_TARGET={}", host_target.display());
+    let target_vendor = std::env::var_os("CARGO_CFG_TARGET_VENDOR").unwrap();
 
     // HACK: Make the interceptor binary actually be a shared dylib, instead of
     // an executable (Cargo only supports installing executables).
-    if cfg!(target_os = "macos") {
+    if target_vendor == "apple" {
         println!("cargo:rustc-link-arg-bin=cargo-sandbox-interceptor=-Wl,-dylib");
     } else {
         println!("cargo:rustc-link-arg-bin=cargo-sandbox-interceptor=-shared");
@@ -17,7 +16,7 @@ fn main() {
     // Embed the current version in the interceptor.
     // TODO: Should we check this against `cargo-sandbox`'s current version?
     let version = env!("CARGO_PKG_VERSION");
-    if cfg!(target_os = "macos") {
+    if target_vendor == "apple" {
         // Read with: `otool -l target/debug/cargo-sandbox-interceptor | sed -n '/cmd LC_ID_DYLIB/,/cmd /p'`
         println!(
             "cargo:rustc-link-arg-bin=cargo-sandbox-interceptor=-Wl,-current_version,{version}"
